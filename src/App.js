25ac9ea210f953e324481711c207c665db752bc2
abanoubmesiha <bony.nagy@gmail.com>
2019-10-25 00:15:50 +0200

    nice grid

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      height:10,
+      width:10,
+
+    }
+    this.handleChangeHeight=this.handleChangeHeight.bind(this);
+    this.handleChangeWidth=this.handleChangeWidth.bind(this);
+    this.renderTableData=this.renderTableData.bind(this);
+  }
+  renderTableData() {
+       return (
+          <tr>
+             <td></td>
+          </tr>
+       )
+    }
+ 
+  handleChangeHeight(event){
+    this.setState({
+      height: event.target.value
+    })
+  }
+  handleChangeWidth(event){
+    this.setState({
+      width: event.target.value
+    })
+  }
+  render() {
+      let rows = [];
+      for (var i = 0; i < this.state.height; i++){
+        let cell = []
+        for (var idx = 0; idx < this.state.width; idx++){
+          cell.push(<td></td>)
+        }
+        rows.push(<tr>{cell}</tr>)
+      }
+    const {height, width} = this.state;
+      return (
         <div className="container">
             <h1>Lab: Pixel Art Maker</h1>
             <h2>Choose Grid Size</h2>
             <form id="sizePicker">
                 Grid Height:
-                <input type="number" id="inputHeight" name="height" min="1" value="10" />
+                <input type="number" id="inputHeight" name="height" min="1"
+                      value={height} onChange={this.handleChangeHeight} />
                 Grid Width:
-                <input type="number" id="inputWidth" name="width" min="1" value="10" />
-                <button id="submit" type="button">Submit</button>
+                <input type="number" id="inputWidth" name="width" min="1"
+                      value={width} onChange={this.handleChangeWidth} />
             </form>
 
             <h2>Pick A Color</h2>
             <input type="color" id="colorPicker" />
-
             <h2>Design Canvas</h2>
-            <table id="pixelCanvas"></table>
+            <table>
+            <thead>
+              
+            </thead>
+            <tbody>
+            {rows}
+            </tbody>
+            <tfoot>
+
+            </tfoot>
+            </table>
         </div>
     );
 }
-
+}
 export default App;
